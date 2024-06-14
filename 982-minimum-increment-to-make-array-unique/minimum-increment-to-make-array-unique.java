f1f1b7a2ffd838c12f5b2e0446435f7c136b3491
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-06-14 22:54:05 +0530

    Time: 37 ms (72.13%) | Memory: 60 MB (64.26%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
         int minAvailable = 0;
         Arrays.sort(nums);
-        for (final int num : nums) {
+        for (int num : nums) {
             ans += Math.max(minAvailable - num, 0);
             minAvailable = Math.max(minAvailable, num) + 1;
         }
