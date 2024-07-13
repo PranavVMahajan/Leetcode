e2473cb964a5624aa9ac70334fd79c67ed12e7ca
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-07-13 23:23:16 +0530

    Time: 39 ms (65.00%) | Memory: 57.4 MB (100.00%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indexes = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indexes[i] = i;
+        }
+        Arrays.sort(indexes, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return positions[a] - positions[b];
+            }
+        });
+        
+        List<Integer> res = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int index: indexes ) {
+            if(directions.charAt(index) == 'R') {
+                stack.push(index);
+            }
+            else { // in - L
+                boolean isSurvived = true;
+                while(!stack.isEmpty()) {
+                    int topIndex = stack.peek();
+                    //case 1: if dir of top is 'L', then insert
+                    if(directions.charAt(topIndex) == 'L') {
+                        isSurvived = true;
+                        break;
+                    }
+                    // case 2: 'topIndex' is 'R' & health is more than 'index'
+                    else if(healths[topIndex] > healths[index]) {
+                        isSurvived = false;
+                        healths[index] = 0;
+                        healths[topIndex] -= 1;
+                        break;
+                    }
+                    // case 3: 'topIndex' is 'R' & health is less than 'index'
+                    else if(healths[index] > healths[topIndex]) {
+                        isSurvived = true;
+                        healths[topIndex] = 0;
+                        healths[index] -= 1;
+                        stack.pop();
+                    }
+                    // case 4: 'topIndex' is 'R' & health is equal to 'index'
+                    else {
+                        isSurvived = false;
+                        healths[topIndex] = 0;
+                        healths[index] = 0;
+                        stack.pop();
+                        break;  
+                    }
+                }
+                if(isSurvived) {
+                    stack.push(index);
+                }
+            }
+        }
+        for(int i=0;i<n;i++) {
+            if(healths[i]>0) {
+                res.add(healths[i]);
+            }
+        }
+        return res;
+    }
+}
