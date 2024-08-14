d48747250986174e27956631a1075c5af52cb425
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-08-14 23:34:41 +0530

    Time: 7 ms (96.26%) | Memory: 44.5 MB (40.48%) - LeetSync

---
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int slidingWindowCount(int[] nums, int D) {
+        int count = 0;
+        int n = nums.length;
+        int i = 0;
+        int j = 1;
+
+        while (j < n) {
+            while (nums[j] - nums[i] > D) {
+                i++;
+            }
+            count += j - i;
+            j++;
+        }
+        return count;
+    }
+
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int l = 0;
+        int r = nums[n - 1] - nums[0];
+        int result = 0;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int count = slidingWindowCount(nums, mid);
+
+            if (count < k) {
+                l = mid + 1;
+            } else {
+                result = mid;  // Store the mid as a potential result
+                r = mid - 1;
+            }
+        }
+        return result;
+    }
+
+}
