94bdda6d132a73e08ece0bd3845501abbf0c63de
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-08-18 21:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (86.83%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isUgly(int n) {
         if(n == 0) return false;
-        while(true) {
-            if(n%2 != 0) {
-                break;
-            }
-            n /=2;
+        if(n<=3 && n > 0) {
+            return true;
         }
-        while(true) {
-            if(n%3 != 0) {
-                break;
-            }
-            n /=3;
+
+        if(n %2 == 0) {
+            return isUgly(n/2);
         }
-        while(true) {
-            if(n%5 != 0) {
-                break;
-            }
-            n /=5;
+        if(n %3 == 0) {
+            return isUgly(n/3);
         }
-        return (n == 1);
-     }
+        if(n %5 == 0) {
+            return isUgly(n/5);
+        }
+        return false;
+    }
 }
