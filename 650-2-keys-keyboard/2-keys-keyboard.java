dd13b8fa34562b7434c98dd08975dcdb361d1c3c
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-08-19 20:55:16 +0530

    Time: 6 ms (57.86%) | Memory: 40.3 MB (76.98%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSteps(int n) {
+        if(n == 1) return 0;
+        if(n == 2) return 2;
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            int factor = i/2;
+            while(factor >= 1) {
+                if(i % factor == 0) {
+                    int stepsToPrintFactorAs = dp[factor];
+                    int copy = 1;
+                    int paste = (i / factor) - 1;
+
+                    dp[i] = stepsToPrintFactorAs + copy + paste;
+                    break;
+                }
+                else factor--;
+            }
+        }
+        return dp[n];
+    }
+}
