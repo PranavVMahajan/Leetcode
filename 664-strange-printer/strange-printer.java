940fdc048cc2a3557d22b51afaae2f6d529f6bee
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-08-22 01:15:54 +0530

    Time: 17 ms (49.59%) | Memory: 43.1 MB (67.89%) - LeetSync

--- 664-strange-printer/strange-printer.java
+++ 664-strange-printer/strange-printer.java
@@ -1,36 +1,43 @@
 class Solution {
-    public int strangePrinter(String s) {
-        int n = s.length();
-        int dp[][] = new int[n+1][n+1]; 
-        for(int[] d: dp) Arrays.fill(d, -1);
+    int n;
+    int[][] dp;
 
-        return solve(0, n-1, s, dp);
-    }
-
-    public int solve(int l, int r, String s, int[][] dp) {
-        if(l == r) return 1;
-        if(l > r) return 0;
-
-        if(dp[l][r] != -1) {
+    public int solve(int l, int r, String s) {
+        if (l == r)
+            return 1;
+        
+        if (l > r)
+            return 0;
+        
+        if (dp[l][r] != -1)
             return dp[l][r];
-        }
 
-        int i = l+1;
-        while(i < r && s.charAt(i) == s.charAt(l)) i++;
+        int i = l + 1;
+        while (i <= r && s.charAt(i) == s.charAt(l))
+            i++;
 
-        if(i == r+1) return 1;
+        if (i == r + 1)
+            return 1;
 
-        int normal = 1 + solve(i, r, s, dp);
+        int normal = 1 + solve(i, r, s);
+        int minTurn = Integer.MAX_VALUE;
 
-        int greedy = Integer.MAX_VALUE;
-
-        for(int j=i; j<=r; j++) {
-            if(s.charAt(j) == s.charAt(l)) {
-                int x = solve(i, j-1, s, dp) + solve(j, r, s, dp);
-                greedy = Math.min(greedy, x);
+        for (int j = i; j <= r; j++) {
+            if (s.charAt(l) == s.charAt(j)) {
+                int x = solve(i, j - 1, s) + solve(j, r, s);
+                minTurn = Math.min(minTurn, x);
             }
         }
 
-        return dp[l][r] = Math.min(normal, greedy);
+        return dp[l][r] = Math.min(minTurn, normal);
+    }
+
+    public int strangePrinter(String s) {
+        n = s.length();
+        dp = new int[n][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return solve(0, n - 1, s);
     }
 }
