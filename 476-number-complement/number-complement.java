4a0b1f0129494597a476e9e07c030df1a28312e7
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-08-22 23:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (97.89%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,9 +1,14 @@
 class Solution {
     public int findComplement(int num) {
-       if( num == 1) return 0;
-       int len = (int)(Math.log(num) / Math.log(2)) + 1;
-       int mask = (1<<len) -1;
-       return mask^num;
-
+        int i = 0;
+        int result = 0;
+        while(num != 0) {
+            if((num & 1) == 0) {
+                result |= (1 << i);
+            }
+            num >>= 1;
+            i++;
+        }
+        return result;
     }
 }
