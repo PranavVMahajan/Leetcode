f64860288004fa8653c22117bf03142648f0e1d9
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-10-03 00:03:30 +0530

    Time: 56 ms (12.64%) | Memory: 62.9 MB (14.71%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
        int n = arr.length;
-       ArrayList<Integer> indexArr = new ArrayList<>();
-       for(int i = 0;i<n;i++) {
-          indexArr.add(i);
-        }
-        Collections.sort(indexArr, new Comparator<Integer>() {
-          public int compare(Integer x, Integer y) {
-                return arr[x] - arr[y]; 
+       //{element,index}
+       PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+          public int compare(int x[], int y[]) {
+                return x[0] - y[0]; 
           }
         });
+       for(int i = 0;i<n;i++) {
+           pq.offer(new int[]{arr[i],i});
+        }
+        
        int rank = 0;
        int prev = Integer.MAX_VALUE;
-       int ans[] = new int[n];
-       for(int idx : indexArr) {
-            if(prev != arr[idx]) {
-                prev = arr[idx];
+    //    for(int idx : indexArr) {
+        while(!pq.isEmpty()) {
+            int cur[] = pq.poll();
+            if(prev != cur[0]) {
+                prev = cur[0];
                 rank++;
             }
-            ans[idx] = rank;
+            arr[cur[1]] = rank;
         }
-        return ans;
+        return arr;
     }
 }
