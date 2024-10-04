f9e57b9ce7e3a1c2b3d37cb11877f1bc26ce5d80
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-10-04 23:53:25 +0530

    Time: 3 ms (96.47%) | Memory: 55.2 MB (97.23%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -3,9 +3,10 @@ class Solution {
         int n = skill.length;
         int teams = n/2;
         int totalSum = 0;
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        int map[] = new int[1001];
         for(int element : skill) {
-            freqMap.put(element,freqMap.getOrDefault(element,0)+1);
+            map[element]++;
+            //freqMap.put(element,freqMap.getOrDefault(element,0)+1);
             totalSum += element;
         }
         if(totalSum%teams !=0) {
@@ -14,15 +15,15 @@ class Solution {
         int targetPoints = totalSum/teams;
         long res = 0;
         for(int element : skill) {
-            if(freqMap.get(element) == 0) {
+            if(map[element]==0) {
                 continue;
             }
-            freqMap.put(element,freqMap.get(element)-1);
+            map[element]--;
             int partner = targetPoints - element;
-            if(!freqMap.containsKey(partner) || freqMap.get(partner) == 0) {
+            if(map[partner] == 0) {
                 return -1;
             }
-            freqMap.put(partner,freqMap.get(partner)-1);
+            map[partner]--;
             res = res + ((long)element * (long) partner);
         }
         return res;
