0718a8560a607245f0b302c5d8c58bdc08e5349f
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2024-10-09 22:00:22 +0530

    Time: 1 ms (58.43%) | Memory: 41.2 MB (79.36%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         Stack<Character> stack = new Stack<>();
-        int count = 0; // To keep track of unmatched closing parentheses
+        int count = 0; 
         for (char ch : s.toCharArray()) {
             if (ch == '(') {
                 stack.push(ch);
             } else if (ch == ')') {
                 if (!stack.isEmpty()) {
-                    stack.pop(); // Match a closing parenthesis with an opening one
+                    stack.pop(); 
                 } else {
-                    count++; // Increment count for unmatched closing parentheses
+                    count++; 
                 }
             }
         }
-        // The stack will contain the unmatched opening parentheses
         return stack.size() + count;
     }
 }
