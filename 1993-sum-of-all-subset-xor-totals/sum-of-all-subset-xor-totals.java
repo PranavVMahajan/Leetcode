7dd49494cc692a35449c53a52eedf7c4ca51316f
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2025-04-06 02:06:36 +0530

    Time: 1 ms (41.21%) | Memory: 41.7 MB (19.36%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -0,0 +1,8 @@
+class Solution {
+      public int subsetXORSum(int[] nums) {
+          return Arrays.stream(nums).reduce((a, b) -> a | b).getAsInt() << nums.length - 1;
+        }
+          
+}
+       
+    
