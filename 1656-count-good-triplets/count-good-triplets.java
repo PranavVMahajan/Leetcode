a4f87517d7032ffa5ba2c988626bbe1c5175f742
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2025-04-14 21:47:27 +0530

    Time: 10 ms (77.35%) | Memory: 41.5 MB (45.60%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.java
+++ 1656-count-good-triplets/count-good-triplets.java
@@ -4,9 +4,11 @@ class Solution {
         int n = arr.length;
         for(int i=0;i<=n-3;i++){
             for(int j=i+1;j<=n-2;j++){
-                for(int k=j+1;k<=n-1;k++){
-                    if(Math.abs(arr[i]-arr[j]) <= a && Math.abs(arr[j]-arr[k]) <= b && Math.abs(arr[i]-arr[k]) <= c ) {
-                        good++;
+                if(Math.abs(arr[i]-arr[j]) <= a ) {
+                    for(int k=j+1;k<=n-1;k++){
+                        if( Math.abs(arr[j]-arr[k]) <= b && Math.abs(arr[i]-arr[k]) <= c ) {
+                            good++;
+                        }
                     }
                 }
             }
