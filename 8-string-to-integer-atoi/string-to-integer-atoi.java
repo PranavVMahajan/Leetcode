929198c408b9e41a878d20e0efc9fb68fbe08773
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2026-02-22 12:50:01 +0530

    Time: 1 ms (99.73%) | Memory: 44.1 MB (12.62%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,25 +1,21 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        int i = 0, n = s.length();
-        while (i < n && s.charAt(i) == ' ') {
+       if (s == null || s.length() == 0) return 0;
+       int i=0; 
+       int n = s.length();
+        while(i < n && s.charAt(i) == ' ') {
             i++;
         }
-
-    
-        int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') sign = -1;
-            i++;
+       int sign = 1;
+       if(i<n  && (s.charAt(i) == '+' || s.charAt(i) =='-')){
+           if (s.charAt(i) == '-') sign = -1;
+           i++;
         }
 
+        long num = 0;
         
-        long num = 0; 
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            num = num * 10 + (s.charAt(i) - '0');
-
-        
+        while(i<n && Character.isDigit(s.charAt(i))){
+            num = num*10+(s.charAt(i)-'0');
             if (sign == 1 && num > Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
@@ -28,7 +24,6 @@ class Solution {
             }
             i++;
         }
-
         return (int)(sign * num);
     }
 }
