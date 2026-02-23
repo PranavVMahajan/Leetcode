1567227794e72084c2689e9bb88554b1a63b1577
Pranav Mahajan <147647546+PranavVMahajan@users.noreply.github.com>
2026-02-23 22:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2) return n;
-        int f = 1,s=2;
-        for(int i=3;i<=n;i++){
-            int third = f+s;
-            f=s;
-            s=third;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return s;
+        return dp[n];
     }
 }
